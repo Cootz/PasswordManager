ffcd9ff3a3a3707e855b08513836111a087b5421
Cootz <artem2004799@gmail.com>
2022-11-19 11:11:56 +0300

    DataGreed style modification

--- Passwords/obj/Debug/net6.0-windows/App.g.cs
+++ Passwords/obj/Debug/net6.0-windows/App.g.cs
@@ -45,7 +45,7 @@ namespace Passwords {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.10.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.0.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace Passwords {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.10.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.0.0")]
         public static void Main() {
             Passwords.App app = new Passwords.App();
             app.InitializeComponent();
