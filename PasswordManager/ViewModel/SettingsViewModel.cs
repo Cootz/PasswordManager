81bfe8a24e3f0cd7b49a322c181bfcf67123aa7c
PC <PC@DESKTOP-HS6125F>
2023-03-07 12:28:55 +0300

    Add settings list

--- PasswordManager/ViewModel/SettingsViewModel.cs
+++ PasswordManager/ViewModel/SettingsViewModel.cs
@@ -1,8 +1,17 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using PasswordManager.Model.DB.Schema;
+using PasswordManager.Services;
 
 namespace PasswordManager.ViewModel
 {
-    public class SettingsViewModel : ObservableObject
+    public partial class SettingsViewModel : ObservableObject
     {
+        [ObservableProperty]
+        private IQueryable<ServiceInfo> serviceInfos;
+
+        public SettingsViewModel(DatabaseService db) 
+        {
+            ServiceInfos = db.Select<ServiceInfo>();
+        }
     }
 }
