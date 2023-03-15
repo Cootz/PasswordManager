e31c9e404d4f0eb16ec36a36621d0552fd0ca309
Cootz <artem2004799@gmail.com>
2023-03-15 12:00:32 +0300

    Make testing work

--- PasswordManager.Tests/DB/DatabaseTest.cs
+++ PasswordManager.Tests/DB/DatabaseTest.cs
@@ -1,4 +1,5 @@
-﻿using PasswordManager.Model.DB;
+﻿using NUnit.Framework;
+using PasswordManager.Model.DB;
 using PasswordManager.Model.DB.Schema;
 using PasswordManager.Services;
 using PasswordManager.Tests.IO;
@@ -6,21 +7,24 @@ using Realms;
 
 namespace PasswordManager.Tests.DB
 {
-
+    [TestFixture]
     public class DatabaseTest
     {
         static TempStorage? tempStorage;
         static RealmController? controller;
         static DatabaseService? database;
 
-        [SetUp]
+        [OneTimeSetUp]
         public static void Setup()
         {
-            tempStorage = new TempStorage();
-            controller = new RealmController(tempStorage);
-            database = new(controller);
+            if (tempStorage is null)
+            {
+                tempStorage = new TempStorage();
+                controller = new RealmController(tempStorage);
+                database = new(controller);
 
-            database.Initialize().Wait();
+                database.Initialize().Wait();
+            }
         }
 
         protected void RunTestWithDatabase(Action<DatabaseService> testRun)
@@ -36,8 +40,33 @@ namespace PasswordManager.Tests.DB
         [TearDown]
         public async Task TearDown()
         {
+            await controller!.RealmQuerry(async (realm) => {
+                await realm.WriteAsync(() =>
+                {
+                    realm.RemoveAll<ProfileInfo>();
+
+                    foreach (ServiceInfo service in realm.All<ServiceInfo>())
+                    { 
+                        if (!ServiceInfo.DefaultServices.Contains(service))
+                            realm.Remove(service);
+                    }
+                });
+
+                while (realm.IsInTransaction)
+                {
+                    await Task.Delay(50);
+                }
+
+                realm.Refresh();
+            });
+        }
+
+        public async Task OneTimeTearDown()
+        {
             await controller!.RealmQuerry(async (realm) =>
             {
+                await realm.RefreshAsync();
+
                 DeleteRealmWithRetries(realm);
             });
         }
