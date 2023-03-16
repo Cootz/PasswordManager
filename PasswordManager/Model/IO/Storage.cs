44e2907be5a052b30239d64e7d5d6fb77ddc93c9
Cootz <artem2004799@gmail.com>
2023-03-16 11:20:14 +0300

    Add comments for `Storage.cs`

--- PasswordManager/Model/IO/Storage.cs
+++ PasswordManager/Model/IO/Storage.cs
@@ -5,6 +5,9 @@
     /// </summary>
     public abstract class Storage
     {
+        /// <summary>
+        /// Current working directory of the storage
+        /// </summary>
         public string WorkingDirectory { get; private set; }
 
         public Storage(string path)
@@ -14,6 +17,14 @@
             WorkingDirectory = path;
         }
 
+        /// <summary>
+        /// Get storage for subdirectory. Creates the directory if not exists
+        /// </summary>
+        /// <param name="directory">Subdirectory for the storage</param>
+        /// <returns>New storage attached to <paramref name="directory"/></returns>
+        /// <remarks>
+        /// <paramref name="directory"/> is a subdirectory of <see cref="WorkingDirectory"/>. It cannot be a path or filename
+        /// </remarks>
         public abstract Storage GetStorageForDirectory(string directory);
     }
 }
