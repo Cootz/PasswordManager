eda209852364bc6a2685a113521c401c5e921cbc
Cootz <artem2004799@gmail.com>
2023-04-01 22:37:12 +0300

    Make 'LoginPage' work

--- PasswordManager/View/LoginPage.xaml.cs
+++ PasswordManager/View/LoginPage.xaml.cs
@@ -1,9 +1,12 @@
+using PasswordManager.ViewModel;
+
 namespace PasswordManager.View;
 
 public partial class LoginPage : ContentPage
 {
-	public LoginPage()
+	public LoginPage(LoginViewModel vm)
 	{
 		InitializeComponent();
+		BindingContext = vm;
 	}
 }
