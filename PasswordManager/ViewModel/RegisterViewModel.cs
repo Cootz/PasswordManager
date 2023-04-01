dd38cb6944ee2976c25896d69a9a2a28b78da41c
Cootz <artem2004799@gmail.com>
2023-04-01 22:45:03 +0300

    Add logic for `RegisterPage`

---
+++ PasswordManager/ViewModel/RegisterViewModel.cs
@@ -0,0 +1,36 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Microsoft.Maui.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordManager.ViewModel
+{
+    public partial class RegisterViewModel : ObservableObject
+    {
+        private ISecureStorage secureStorage;
+
+        [ObservableProperty]
+        private string password;
+
+        [ObservableProperty]
+        private string passwordConfirmation;
+
+        public RegisterViewModel(ISecureStorage secureStorage)
+        { 
+            this.secureStorage = secureStorage;
+        }
+
+        async void Register()
+        {
+            if (Password == PasswordConfirmation && Password.Length >= 8)
+            {
+                await secureStorage.SetAsync("app-password", Password);
+
+                Application.Current.MainPage = new AppShell();
+            }
+        }
+    }
+}
