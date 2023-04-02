5650c06e0be0363cc441583f2404920ea3bf4ef0
Cootz <artem2004799@gmail.com>
2023-04-03 02:54:52 +0300

    Add tests for `LoginViewModel`

---
+++ PasswordManager.Tests/ViewModel/LoginViewModelTest.cs
@@ -0,0 +1,62 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using NSubstitute;
+using PasswordManager.Services;
+using PasswordManager.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordManager.Tests.ViewModel
+{
+    [TestFixture]
+    public class LoginViewModelTest
+    {
+        private ISecureStorage secureStorage = Substitute.For<ISecureStorage>();
+        private INavigationService navigationService = Substitute.For<INavigationService>();
+        private const string password = "TestP@ssw0rd";
+
+        private bool pageChanged = false;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            secureStorage.GetAsync("app-password").Returns(password);
+            navigationService.NavigateToAsync(default).ReturnsForAnyArgs(Task.CompletedTask).AndDoes((task) => pageChanged = true);
+        }
+
+        [Test]
+        public void TestLoginWithCorrectPassword()
+        { 
+            string enteredPassword = password;
+
+            LoginViewModel viewModel = new(secureStorage, navigationService);
+            var command = viewModel.LoginCommand;
+
+            viewModel.Password = enteredPassword;
+
+            command.Execute(null);
+
+            Assert.That(pageChanged, Is.True);
+        }
+
+        [Test]
+        public void TestLoginWithIncorrectPassword()
+        {
+            string enteredPassword = "wrongPassword";
+
+            LoginViewModel viewModel = new(secureStorage, navigationService);
+            var command = viewModel.LoginCommand;
+
+            viewModel.Password = enteredPassword;
+
+            command.Execute(null);
+
+            Assert.That(pageChanged, Is.False);
+        }
+
+        [TearDown]
+        public void TearDown() => pageChanged = false;
+    }
+}
--- PasswordManager/ViewModel/LoginViewModel.cs
+++ PasswordManager/ViewModel/LoginViewModel.cs
@@ -24,7 +24,7 @@ namespace PasswordManager.ViewModel
         {
             if (Password == await secureStorage.GetAsync("app-password"))
             {
-                await Shell.Current.GoToAsync($"//{nameof(RecentPage)}");
+                await navigationService.NavigateToAsync($"//{nameof(RecentPage)}");
 
 #if __MOBILE__
                 AppShell.SetFlyoutBehavior(AppShell.Current, FlyoutBehavior.Flyout);
