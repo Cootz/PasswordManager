1b61d27b63b14196fff908e2b1b5dfa2401ab0f6
Cootz <artem2004799@gmail.com>
2023-04-18 18:16:34 +0300

    Add ability to trigger login by pressing enter

--- PasswordManager/ViewModel/LoginViewModel.cs
+++ PasswordManager/ViewModel/LoginViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using Microsoft.Maui.ApplicationModel;
 using PasswordManager.Services;
 using PasswordManager.View;
+using SharpHook;
 
 namespace PasswordManager.ViewModel
 {
@@ -17,6 +19,18 @@ namespace PasswordManager.ViewModel
         {
             this.secureStorage = secureStorage;
             navigationService = navigation;
+
+            var hook = new TaskPoolGlobalHook();
+
+            hook.KeyPressed += OnKeyPressed;
+
+            hook.RunAsync();
+        }
+
+        private void OnKeyPressed(object sender, KeyboardHookEventArgs e)
+        {
+            if (e.Data.KeyCode == SharpHook.Native.KeyCode.VcEnter)
+               MainThread.BeginInvokeOnMainThread(Login);
         }
 
         [RelayCommand]
