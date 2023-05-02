6303348d83ef0e88f6e861fef344db45f487cbed
Cootz <artem2004799@gmail.com>
2023-05-02 09:36:45 +0300

    Add `ISettingsService` and `SettingsService`

---
+++ PasswordManager/Services/SettingsService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PasswordManager.Model.DB.Schema;
+
+namespace PasswordManager.Services
+{
+    public class SettingsService : ISettingsService
+    {
+        private readonly DatabaseService databaseService;
+
+        public IQueryable<ProfileInfo> ProfileInfos
+        {
+            get => databaseService.Select<ProfileInfo>();
+        }
+
+        public AppTheme CurrentTheme
+        {
+            get => Preferences.Default.Get("app_theme", AppTheme.Unspecified); 
+            set => Preferences.Default.Set("app_theme", value);
+        }
+
+        public SettingsService(DatabaseService databaseService)
+        {
+            this.databaseService = databaseService;
+        }
+    }
+}
