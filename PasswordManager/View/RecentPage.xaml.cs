ad80c21b762eac689ec56399efbd5692155080ce
Cootz <artem2004799@gmail.com>
2023-05-22 05:17:03 +0300

    Remove swipe vies as it's bugged out

--- PasswordManager/View/RecentPage.xaml.cs
+++ PasswordManager/View/RecentPage.xaml.cs
@@ -6,14 +6,8 @@ public partial class RecentPage : ContentPage
 {
     public RecentPage(RecentViewModel vm)
     {
-        InitializeComponent();
         BindingContext = vm;
 
-        //Swipeview doesn't work in WinUI due to the bug. See: https://github.com/dotnet/maui/issues/8870
-#if WINDOWS
-        ProfilesCollectionView.ItemTemplate = (DataTemplate)Resources["WindowsSpecific"];
-#else
-        ProfilesCollectionView.ItemTemplate = (DataTemplate)Resources["General"];
-#endif
+        InitializeComponent();
     }
 }
