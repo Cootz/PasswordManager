113cf93ccab11a2020a3df7e8cfca86e83b6ea00
Cootz <artem2004799@gmail.com>
2023-05-23 05:50:50 +0300

    Add `ToastService`

---
+++ PasswordManager/Services/IToastService.cs
@@ -0,0 +1,9 @@
+﻿using CommunityToolkit.Maui.Core;
+
+namespace PasswordManager.Services
+{
+    public interface IToastService
+    {
+        IToast Make(string message, ToastDuration duration = ToastDuration.Short, double textSize = 14);
+    }
+}
---
+++ PasswordManager/Services/ToastService.cs
@@ -0,0 +1,10 @@
+﻿using CommunityToolkit.Maui.Alerts;
+using CommunityToolkit.Maui.Core;
+
+namespace PasswordManager.Services
+{
+    public class ToastService : IToastService
+    {
+        public IToast Make(string message, ToastDuration duration = ToastDuration.Short, double textSize = 14) => Toast.Make(message, duration, textSize);
+    }
+}
