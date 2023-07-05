a53fe349a6478471b6f83cb37d9a3084a381c588
Cootz <artem2004799@gmail.com>
2023-07-05 08:23:03 +0300

    Show hidden password first. Reveal on tap

--- PasswordManager/ViewModel/ProfileViewModel.cs
+++ PasswordManager/ViewModel/ProfileViewModel.cs
@@ -7,13 +7,39 @@ namespace PasswordManager.ViewModel;
 [QueryProperty(nameof(ProfileInfo), "profile")]
 public partial class ProfileViewModel : ObservableObject
 {
+    public const string HIDDEN_PASSWORD = "*****";
+
     [ObservableProperty] private ProfileInfo profileInfo;
+
+    private bool isPasswordVisible;
+
+    public bool IsPasswordVisible
+    {
+        get => isPasswordVisible;
+        set
+        {
+            if (isPasswordVisible == value)
+                return;
+
+            OnPropertyChanging(nameof(VisiblePassword));
+            isPasswordVisible = value;
+            OnPropertyChanged(nameof(VisiblePassword));
+        }
+    }
 
     public string PageTitle
     {
         get => $"Profile {ProfileInfo?.Username ?? string.Empty}";
     }
 
+    public string VisiblePassword
+    {
+        get => IsPasswordVisible ? ProfileInfo.Password : HIDDEN_PASSWORD;
+    }
+
     [RelayCommand]
     void CopyToClipboard(string text) => Clipboard.Default.SetTextAsync(text);
+
+    [RelayCommand]
+    void ChangePasswordVisibility() => IsPasswordVisible = !IsPasswordVisible;
 }
