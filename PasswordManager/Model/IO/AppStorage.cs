365afebc7f2b3d4d35ffc342680f257ea885a3d2
Artem.Matveev <artem.matveev.euroml@rambler.ru>
2023-11-11 12:07:42 +0300

    Add comments for `AppStorage`

--- PasswordManager/Model/IO/AppStorage.cs
+++ PasswordManager/Model/IO/AppStorage.cs
@@ -1,5 +1,8 @@
 ﻿namespace PasswordManager.Model.IO;
 
+/// <summary>
+/// Represents storage for the application
+/// </summary>
 public class AppStorage : Storage
 {
     public AppStorage(string path) : base(path)
